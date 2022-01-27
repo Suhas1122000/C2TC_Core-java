48708bd1575e470797b7dc169899b4ed0583c397
Suhas <Suhas@DESKTOP-LTBNR3A>
2022-01-27 10:56:51 +0530

    first

---
+++ ss/src/ss/er.java
@@ -0,0 +1,16 @@
+package ss;
+
+public class er {
+
+	public static void main(String[] args) {
+		int[] scores = new int[10];
+	    int sc[]= new int[3];
+	   int[] scores1= {215, 234, 218, 189, 221, 290, 25, 56, 12, 45, 56};
+		for(int score : scores1)
+		{
+			
+		System.out.println(score+ " ");
+	    }
+	}
+
+}
---
+++ ss/src/ss/s1.java
@@ -0,0 +1,15 @@
+package ss;
+import java.util.Scanner;
+
+public class s1 {
+
+	public static void main(String[] args) {
+		Scanner var = new Scanner(System.in);
+		System.out.println("enter the name ");{
+			String name= var.nextLine();
+			System.out.println(name);
+		}
+
+	}
+
+}
